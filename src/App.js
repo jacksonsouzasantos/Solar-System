ce5da8b4d9166df96e1ffdd70ec1814c6e14a2d3
Jackson Souza Santos <jacksonsantos.tec@gmail.com>
2022-11-28 17:22:59 -0300

    estilizando css-backgroud-textos-h1-h2

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Header from './components/Header';
 import PlanetCard from './components/PlanetCard';
 import SolarSystem from './components/SolarSystem';
 import Missions from './components/Missions';
+import './App.css';
 
 class App extends React.Component {
   render() {
